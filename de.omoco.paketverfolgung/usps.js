1f8b8ca4210e98e4c608596a40750e0d15518245
Donald Kirker <donald.kirker@openmobl.com>
2014-07-21 01:02:37 -0700

    Update USPS service.

--- de.omoco.paketverfolgung/usps.js
+++ de.omoco.paketverfolgung/usps.js
@@ -37,50 +37,69 @@ USPS.prototype.getDetails = function() {
 USPS.prototype.getDetailsRequestSuccess = function(response) {
 	var responseText = response.responseText;
 	
-	var responseText2 = responseText.split("<div class=\"progress-indicator\">")[1];
-	responseText2 = responseText2.split("<h2 class=\"hide-fromsighted\">")[1];
+	//var responseText2 = responseText.split("<tbody class=\"details\">")[1];
+	var statusText = responseText.split("<div class=\"progress-indicator\">")[1].split("<h2 class=\"hide-fromsighted\">")[1].split("</h2>")[0];
+
+	// Real USPS statuses:
+	//<div class="progress-indicator">
+	//	<h2 class="hide-fromsighted">in-transit</h2>
+	//</div>
+	//
+	// Values and Package Tracker values:
+	// pre-shipment: 1
+	// accepted: 2
+	// in-transit: 3
+	// in-transit + special: 4
+	// delivered: 5
+	// error: -1
+	// seized: -1
+	// archived: -1
+
 
 	var status = 0;
-	if(responseText2.split("Accept").length > 1 || responseText2.split("Info Received").length > 1 || responseText2.split("Picked Up").length > 1) {
+	if (statusText.indexOf("pre-shipment") != -1) {
 		status = 1;
-	}
-	if (responseText2.split("GIBTS ES WOHL NICHT").length > 1 || responseText2.split("Arrived Shipping Partner Facility").length > 1) {
+	} else if (statusText.indexOf("accepted") != -1) {
 		status = 2;
-	}
-	if (responseText2.split("Process").length > 1 || responseText2.split("Depart").length > 1 || responseText2.split("Sorting").length > 1) {
+	} else if (statusText.indexOf("in-transit") != -1) {
 		status = 3;
-	}
-	if (responseText2.split("Delivery").length > 1) {
+	} /*else if (statusText.indexOf("out-for-delivery") != -1) {
 		status = 4;
-	}
-	if (responseText2.split("Delivered").length > 1) {
+	}*/ else if (statusText.indexOf("delivered") != -1) {
 		status = 5;
-	}
-	if (responseText2.split("Sorry, there's no information for that number.").length > 1 || responseText2.split("Delivery status information is not available").length > 1) {
+	} else {
 		status = -1;
 	}
-
-	this.callbackStatus(status);
 
 	if (status > 0) {
 		var details = [];
 		
 		var detailsText = responseText.split("detail-wrapper");
 		for (var i = 1; i < detailsText.length; i++) {
-			var detailsSplit = detailsText[i].split("<p");
+			var detailsSplit = detailsText[i].split("<td class=\"date-time\">");
 
-			// > Want the text here and strip non-date/time characters </p>
-			var tmpDateStr = detailsSplit[1].split("</p>")[0].split(">")[1].replace(/[\r]/g, " ").replace(/[^,: a-zA-Z0-9]/g, "").trim();
+			var tmpDateStr = detailsSplit[1].split("<p>")[1].split("</p>")[0].replace(/[\r]/g, " ").replace(/[^,: a-zA-Z0-9]/g, "").trim();
 
-			// > Want the text here </p>
-			var tmpLoc = detailsSplit[3].split("</p>")[0].split(">")[1].replace(/[\t\r]/g, "").trim();
-
-			// info-text"> Want the text here </span>
-			var tmpNotes = detailsText[i].split("info-text\">")[1].split("</span>")[0].replace(/[\t\r]/g, "").trim();
+			var tmpLoc = "";
+			var tmpNotes = "";
+			if (i == 1) {
+				tmpLoc = detailsSplit[1].split("<td class=\"location\">")[1].split("<p>")[1].split("</td>")[0].replace(/[\r]/g, " ").replace(/&nbsp;/g, " ").trim();
+				tmpNotes = detailsSplit[1].split("<td class=\"status\">")[1].split("<p")[1].split("</p>")[0].split(">")[1].replace(/[\r]/g, " ").replace(/&nbsp;/g, " ").trim();
 			
+				if (tmpNotes.indexOf("Out for Delivery") != -1) {
+					status = 4;
+				} else if (tmpNotes.indexOf("Delivery status not updated") != -1) {
+					status = 0;
+				}			
+			} else {
+                tmpLoc = detailsSplit[1].split("<td class=\"location\">")[1].split("<p>")[1].split("</p>")[0].replace(/[\r]/g, " ").replace(/&nbsp;/g, " ").trim();
+                tmpNotes = detailsSplit[1].split("<td class=\"status\">")[1].split("<span")[1].split("</span>")[0].split(">")[1].replace(/[\r]/g, " ").replace(/&nbsp;/g, " ").trim();
+			}
+
 			details.push({date: tmpDateStr, location: tmpLoc, notes: tmpNotes});
 		}
 		
+		this.callbackStatus(status);
 		this.callbackDetails(details.clone());	
 	}
 };
