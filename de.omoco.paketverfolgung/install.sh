2e4f53540ede8bb80a806c07560ed9407d817df1
Donald Kirker <donald@smartthings.com>
2018-07-12 13:26:29 -0700

    Delete old Package Tracker files. See https://github.com/dkirker/webos-app_de.omoco.paketverfolgung

--- de.omoco.paketverfolgung/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT=$1
-INSTALLPATH=/media/cryptofs/apps/usr/palm/applications/de.omoco.paketverfolgung/services
-
-echo "Installing $SCRIPT to $INSTALLPATH/$SCRIPT on device"
-
-novacom put file://$INSTALLPATH/$SCRIPT < $SCRIPT
-
